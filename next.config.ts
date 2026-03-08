ec03c68b65fa2b81c60fee2ff2dc2a2c2e704514
Silhouettete <kruxlotus@gmail.com>
2026-03-08 18:34:58 +0700

    Environment variable updates

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,11 @@ const nextConfig: NextConfig = {
   experimental: {
     useCache: true,
   },
+  env: {
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
+      process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+  },
 };
 
 export default nextConfig;
